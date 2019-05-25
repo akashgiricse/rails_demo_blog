f16ad6b11abd977e0674b3f6859cea88e038f65b
akashgiricse <akashgiricse@gmail.com>
2019-05-25 11:13:16 +0530

    Add comments and refactor code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_040050) do
+ActiveRecord::Schema.define(version: 2019_05_25_041519) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
